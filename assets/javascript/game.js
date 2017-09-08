d3dcfb7ee7f0caad607897bfe80caeda4631b87d
Trevor Meadows <tlm04070@gmail.com>
2017-09-07 22:27:30 -0400

    added the input checking

---
+++ assets/javascript/game.js
@@ -0,0 +1,108 @@
+var word = ['Arizona Cardinals',
+	'Chicago Bears',
+	'Green Bay Packers',
+	'New York Giants',
+	'Detroit Lions',
+	'Washington Redskins',
+	'Philadelphia Eagles',
+	'Pittsburgh Steelers',
+	'Los Angeles Rams',
+	'Cleveland Browns',
+	'Indianapolis Colts',
+	'Dallas Cowboys',
+	'Kansas City Chiefs',
+	'Los Angeles Chargers',
+	'Denver Broncos',
+	'New York Jets',
+	'New England Patriots',
+	'Oakland Raiders',
+	'Tennessee Titans',
+	'Buffalo Bills',
+	'Minnesota Vikings',
+	'Atlanta Falcons',
+	'Miami Dolphins',
+	'New Orleans Saints',
+	'Cincinnati Bengals',
+	'Seattle Seahawks',
+	'Tampa Bay Buccaneers',
+	'Carolina Panthers',
+	'Jacksonville Jaguars',
+	'Baltimore Ravens',
+	'Houston Texans'
+];
+
+
+
+
+
+
+var picked = word[Math.floor(Math.random() * word.length)].toUpperCase();
+
+
+var answerArray = [];
+
+
+
+console.log("Current word is: " + picked);
+
+
+for (i = 0; i < picked.length; i++) {
+	answerArray.push("__");
+}
+
+printBoard();
+
+
+var correctBool = false;
+document.onkeyup = function (event) {
+	var temp = event.key;
+	var userKey = temp.toUpperCase();
+	
+	console.log(userKey);
+	console.log(picked);
+
+
+	for (var i = 0; i < picked.length; i++) {
+		if (userKey === picked[i]) {
+			answerArray[i] = userKey;
+			printBoard();
+			correctBool = true;
+		} 
+	} 
+	if (correctBool === false){
+		decreaseGuesses();
+
+	}
+
+	correctBool = false;
+}
+ 
+
+
+
+
+
+var wins = 0;
+document.getElementById("wins").innerHTML = wins;
+
+
+
+function printBoard() {
+document.getElementById("current").innerHTML = answerArray.join(" ");
+
+
+}
+
+
+
+
+var guesses = 12;
+
+function decreaseGuesses(){
+	guesses--;
+document.getElementById("guesses").innerHTML = guesses;
+}
+
+
+
+document.getElementById("wrong").innerHTML = 'A B C D'
